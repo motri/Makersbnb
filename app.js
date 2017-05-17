a587e0aaa54f0283a92307d569697ce1a94cc6c9
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-05-17 11:48:35 +0100

    User routing added and app connected to database via mongoose

--- app.js
+++ app.js
@@ -4,14 +4,26 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', function() {
+  console.log('connected to database ' + config.database);
+});
+
+mongoose.connection.on('error', function(err) {
+  console.log('database error ' + err);
+});
 
 const app = express();
 
 const users = require('./routes/users');
 
-const port = 3000;
+const port = 2368;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
 app.use('/users', users);
@@ -19,8 +31,6 @@ app.use('/users', users);
 app.get('/', function(req, res) {
   res.send('HOMPAAAGE!!');
 });
-
-
 
 app.listen(port, function() {
   console.log('Server started on port '+ port)
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/signup', function(req, res, next) {
-  res.send('SIGN UP NOW!!!!!!!!!!')
+  res.send('SIGN UP NOW!!!!!!!!!!');
+});
+
+router.get('/authenticate', function(req, res, next) {
+  res.send('AUTHENTICATE');
+});
+
+router.get('/profile', function(req, res, next) {
+  res.send('PROFILE');
+});
+
+router.get('/validate', function(req, res, next) {
+  res.send('VALIDATE');
 });
 
 module.exports = router;
