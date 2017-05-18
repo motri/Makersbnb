bdb59336b1100820f5cb9892432f0bbbb817282f
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-05-18 11:41:19 +0100

    signup complete. Signin form in place, not yet connected to server.

--- angular-src/src/app/components/signup/signup.component.ts
+++ angular-src/src/app/components/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service'
+import { AuthService } from '../../services/auth.service'
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -12,7 +14,12 @@ export class SignupComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private validateService: ValidateService, private flashMessage:FlashMessagesService) { }
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage:FlashMessagesService,
+    private authService:AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -33,5 +40,17 @@ export class SignupComponent implements OnInit {
       this.flashMessage.show('Please use a valid email', { cssClass: 'alert-danger', timeout: 3000 });
       return false;
     }
+
+    this.authService.signupUser(user).subscribe(data => {
+      if(data.success){
+        this.flashMessage.show('You are now signed up and can sign in', { cssClass: 'alert-success', timeout: 3000 });
+        this.router.navigate(['/signin']);
+      } else {
+        this.flashMessage.show('Something went wrong', { cssClass: 'alert-danger', timeout: 3000 });
+        this.router.navigate(['/signup']);
+      }
+    });
+
+
   }
 }
