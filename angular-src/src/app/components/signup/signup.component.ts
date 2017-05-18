3a1b2609d8be658c59be20f0d642ba9075076906
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-05-18 10:48:09 +0100

    Adds angular flash messages, for signup form

--- angular-src/src/app/components/signup/signup.component.ts
+++ angular-src/src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service'
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  name: String;
+  email: String;
+  password: String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService, private flashMessage:FlashMessagesService) { }
 
   ngOnInit() {
   }
 
+  onSignupSumbit(){
+    const user = {
+      name: this.name,
+      email: this.email,
+      password: this.password
+    }
+
+    if(!this.validateService.validateSignup(user)){
+      this.flashMessage.show('Please fill in all fields', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+
+    if(!this.validateService.validateEmail(user.email)){
+      this.flashMessage.show('Please use a valid email', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+  }
 }
