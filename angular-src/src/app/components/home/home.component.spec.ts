34e50246c76687213117a0e45462bea7274b39f3
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-05-19 13:03:27 +0100

    Passess initial HomeComponent unit tests

--- angular-src/src/app/components/home/home.component.spec.ts
+++ angular-src/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { APP_BASE_HREF } from '@angular/common';
+
+const appRoutes: Routes = [
+  {path:'', component: HomeComponent}]
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +12,13 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [
+        RouterModule.forRoot(appRoutes)
+      ],
+      providers:[
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +32,14 @@ describe('HomeComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render h1 title containing MAKERS BnB', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('MAKERS BnB');
+  }));
+
+  it('should render paragraph ', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain('Sulaiman');
+  }));
 });
